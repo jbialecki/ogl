7c73fc87ab65814ebce0a1d384c085cfd7c127f8
Jaroslaw Bialecki <j.bialecki@samsung.com>
2016-12-09 17:43:04 +0100

    Enclosed view in function

--- tutorial03_matrices/tutorial03.cpp
+++ tutorial03_matrices/tutorial03.cpp
@@ -16,6 +16,31 @@ using namespace glm;
 
 #include <common/shader.hpp>
 
+void setView(int MatrixID)
+{
+static float f = 0.0;
+f+= 0.1;
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+						glm::vec3(4+f,3,3), // Camera is at (4,3,3), in World Space
+						glm::vec3(0,0,0), // and looks at the origin
+						glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+					   );
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model      = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	// Send our transformation to the currently bound shader, 
+	// in the "MVP" uniform
+	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+}
+
+
 int main( void )
 {
 	// Initialise GLFW
@@ -80,9 +105,7 @@ int main( void )
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	float f = 0;
 	do{
-		f += 0.1;
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
@@ -91,26 +114,9 @@ int main( void )
 
 
 
+		setView(MatrixID);
 
 
-		// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-		glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-
-		// Camera matrix
-		glm::mat4 View       = glm::lookAt(
-							glm::vec3(4+f,3,3), // Camera is at (4,3,3), in World Space
-							glm::vec3(0,0,0), // and looks at the origin
-							glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
-		// Model matrix : an identity matrix (model will be at the origin)
-		glm::mat4 Model      = glm::mat4(1.0f);
-		// Our ModelViewProjection : multiplication of our 3 matrices
-		glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
-
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
@@ -123,6 +129,7 @@ int main( void )
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
+
 
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
